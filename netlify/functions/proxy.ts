00af97c7730856f4efa3a5846fe536649eaac754
kelepc <ygcg16@gmail.com>
2024-04-27 23:23:19 +0800

    4

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -3,12 +3,12 @@ import { Context } from "@netlify/edge-functions";
 const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
   const picked = new Headers();
   for (const key of headers.keys()) {
-    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-      const value = headers.get(key);
-      if (typeof value === "string") {
-        picked.set(key, value);
-      }
+    // if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
+    const value = headers.get(key);
+    if (typeof value === "string") {
+      picked.set(key, value);
     }
+    // }
   }
   return picked;
 };
